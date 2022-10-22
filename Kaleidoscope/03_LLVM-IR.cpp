1fb6a2cd6292802b3f20ebfdc1295d616e58ed80
Shadow <luxiaodong@me.com>
2022-10-22 11:27:15 +0800

    添加 03 llvm, 连接成功

--- Kaleidoscope/03_LLVM-IR.cpp
+++ Kaleidoscope/03_LLVM-IR.cpp
@@ -1,4 +1,16 @@
 
+#include <llvm/ADT/APFloat.h>
+#include <llvm/ADT/STLExtras.h>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Type.h>
+#include <llvm/IR/Verifier.h>
+
 #include <iostream>
 #include <vector>
 #include <map>
@@ -106,6 +118,7 @@ class ExprAST
 {
 public:
     virtual ~ExprAST() {}
+//    virtual Value *codegen() = 0;
 };
 
 // NumberExprAST - Expression class for numeric literals like "1.0".
--- Kaleidoscope/main.cpp
+++ Kaleidoscope/main.cpp
@@ -1,6 +1,7 @@
 
 //#include "01_Lexer.cpp"
-#include "02_Parser-And-AST.cpp"
+//#include "02_Parser-And-AST.cpp"
+#include "03_LLVM-IR.cpp"
 
 // http://www.tuhaoxin.cn:8080/articles/2019/10/01/1569927157476.html
 
